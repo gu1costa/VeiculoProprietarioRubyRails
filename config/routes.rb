6a6b1ee94c4fd3342e593d857e8856a9ceb30d1e
Guilherme Ferreira <guilhermefecone@gmail.com>
2026-01-12 10:49:43 -0300

    Rotas configuradas como 'nesteds' em routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :veiculos
+  resources :proprietarios do
+    resources :veiculos
+  end
+
+  root "proprietarios#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
